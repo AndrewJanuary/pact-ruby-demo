d5fe47914be0bd2fe8dc2fb54be7f5a169059d51
Andrew January <andrew.january@simplybusiness.co.uk>
2022-07-29 15:33:17 +0100

    Adds consumer class, consumer side test and pact file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,14 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+
+# require test dependencies
+require "pact/message"
+
+# load additional helpers
+require_relative 'support/pact_spec_helper'
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
